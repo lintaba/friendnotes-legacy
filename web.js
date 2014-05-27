f2099daa331f6f8f01a2cba025ff04be036f027a
lintaba <lintaba@gmail.com>
2014-05-27 13:58:21 +0200

    asd

--- web.js
+++ web.js
@@ -1,111 +1,19 @@
 var async   = require('async');
 var express = require('express');
 var util    = require('util');
+var pg = require('pg');
 
-// create an express webserver
-var app = express.createServer(
-  express.logger(),
-  express.static(__dirname + '/public'),
-  express.bodyParser(),
-  express.cookieParser(),
-  // set this to a secret value to encrypt session cookies
-  express.session({ secret: process.env.SESSION_SECRET || 'secret223' }),
-  require('faceplate').middleware({
-    app_id: process.env.FACEBOOK_APP_ID,
-    secret: process.env.FACEBOOK_SECRET,
-    scope:  'user_likes,user_photos,user_photo_video_tags'
-  })
-);
+var app = express();
 
-// listen to the PORT given to us in the environment
 var port = process.env.PORT || 3000;
 
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
 
-var pgclient,pg = require('pg');
-
+var pgclient;
 pg.connect(process.env.DATABASE_URL||"postgres://sqgkvzosmjvier:yUqd8Z4m_4yVk_jwLq-8sQx41l@ec2-54-221-227-25.compute-1.amazonaws.com:5432/d24odihcp1223b", function(err, client) {
   pgclient=client;
 });
 
 
-app.dynamicHelpers({
-  'host': function(req, res) {
-    return req.headers['host'];
-  },
-  'scheme': function(req, res) {
-    return req.headers['x-forwarded-proto'] || 'http';
-  },
-  'url': function(req, res) {
-    return function(path) {
-      return app.dynamicViewHelpers.scheme(req, res) + app.dynamicViewHelpers.url_no_scheme(req, res)(path);
-    }
-  },
-  'url_no_scheme': function(req, res) {
-    return function(path) {
-      return '://' + app.dynamicViewHelpers.host(req, res) + (path || '');
-    }
-  },
-});
-
-function render_page(req, res) {
-  req.facebook.app(function(err, app) {
-    req.facebook.me(function(user) {
-      res.render('index.ejs', {
-        layout:    false,
-        req:       req,
-        app:       app,
-        user:      user
-      });
-    });
-  });
-}
-/*
-function handle_facebook_request(req, res) {
-
-  // if the user is logged in
-  if (req.facebook.token) {
-
-    async.parallel([
-      function(cb) {
-        // query 4 friends and send them to the socket for this socket id
-        req.facebook.get('/me/friends', { limit: 4 }, function(friends) {
-          req.friends = friends;
-          cb();
-        });
-      },
-      function(cb) {
-        // query 16 photos and send them to the socket for this socket id
-        req.facebook.get('/me/photos', { limit: 16 }, function(photos) {
-          req.photos = photos;
-          cb();
-        });
-      },
-      function(cb) {
-        // query 4 likes and send them to the socket for this socket id
-        req.facebook.get('/me/likes', { limit: 4 }, function(likes) {
-          req.likes = likes;
-          cb();
-        });
-      },
-      function(cb) {
-        // use fql to get a list of my friends that are using this app
-        req.facebook.fql('SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1', function(result) {
-          req.friends_using_app = result;
-          cb();
-        });
-      }
-    ], function() {
-      render_page(req, res);
-    });
-
-  } else {
-    render_page(req, res);
-  }
-}
-*/
 app.get('/', function(req,res){
   pgclient.query('select * from db',function(err,res){
     if(err){
@@ -117,3 +25,9 @@ app.get('/', function(req,res){
 });
 app.post('/save', function(req,res){res.write(req.param("uid"));res.end("eol");});
 app.get('/save', function(req,res){res.write(req.param("uid"));res.end("eol");});
+
+
+
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
