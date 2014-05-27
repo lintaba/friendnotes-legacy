111dce4a7df2822b9c71908eab51463af3124ec7
lintaba <lintaba@gmail.com>
2014-05-27 15:04:17 +0200

    asd

--- web.js
+++ web.js
@@ -1,21 +1,44 @@
-
 var pg = require('pg');
+var url = require('url');
 
 var port = process.env.PORT || 3000;
 
 
 var pgclient;
-var params = { host: 'ec2-54-221-227-25.compute-1.amazonaws.com',port:5432,user: 'sqgkvzosmjvier',password: 'yUqd8Z4m_4yVk_jwLq-8sQx41l',database: 'd24odihcp1223b',ssl: true };
+var params = {
+  host: 'ec2-54-221-227-25.compute-1.amazonaws.com',
+  port: 5432,
+  user: 'sqgkvzosmjvier',
+  password: 'yUqd8Z4m_4yVk_jwLq-8sQx41l',
+  database: 'd24odihcp1223b',
+  ssl: true
+};
 var client = new pg.Client(params);
 client.connect();
-console.log(client);
 
-require("http").createServer(function(req,res){res.write(JSON.stringify(client));res.end("hi");}).listen(port);
-console.log("listening on ",port);
+require("http").createServer(function(req, res) {
+  var url_parts = url.parse(request.url, true);
+  var query = url_parts.query;
+  if (query.ownid > 0 && query.uid > 0 && !query.save) {
+    client.query("select * from mydata where ownid=" + query.ownid | 0 + " and uid=" + query.uid | 0, function(err, res) {
+        if (err) {
+          res.end("sql hiba");
+        } else {
+          res.end("siker:" + JSON.stringify(result.rows[0]));
+      }
+    }
+  }else if(query.ownid > 0 && query.uid > 0 && !query.save){
+    res.end("saveing...");
+  }else{
+    res.end("hi");
+  }
+
+}).listen(port);
+console.log("listening on ", port);
 
 /*
 app.get('/', function(req,res){
-  client.query('select * from db',function(err,res){
+  client.query('select mydata from db',function(err,res){
     if(err){
       res.end("hiba");
     }else{
