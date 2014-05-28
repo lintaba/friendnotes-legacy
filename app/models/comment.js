974f13e4b3b5800aef93d577a4d40f1a4a45e009
lintaba <lintaba@gmail.com>
2014-05-28 05:56:20 +0200

    s

--- app/models/comment.js
+++ app/models/comment.js
@@ -13,15 +13,14 @@ function s(t){return t&&t.replace?t.replace(/['\\]/g,""):"";}
 var cache={};
 
  var load=function load(data,callback){
-    if(cache[data.ownid] && cache[data.ownid][data.uid]){
-      return callback(0,cache[data.ownid][data.uid]);
+    if(cache[data.ownid+":"+data.uid]){
+      return callback(0,cache[data.ownid+":"+data.uid]);
     }
   	client.query("SELECT comment,uid FROM comments WHERE uid='"+s(data.uid)+"' and ownid='"+s(data.ownid)+"'", function(err, result) {
   	  if(err){console.log("0",err); return callback(err);}
 	    	callback(0,result.rows[0]||{});
         if(result.rows[0]){
-          if(!cache[data.ownid]){cache[data.ownid]={};}
-          cache[data.ownid][data.uid]=result.rows[0];
+          cache[data.ownid+":"+data.uid]=result.rows[0];
         }
   	});
   }
@@ -34,7 +33,7 @@ var save=  function save(data,callback){
   		}else{
   			q="insert into comments values ('"+s(data.ownid)+"','"+s(data.uid)+"','"+s(data.comment)+"',NOW())";
   		}
-      delete cache[data.ownid][data.uid];
+      if(cache[data.ownid+":"+data.uid]) delete cache[data.ownid+":"+data.uid];
   		client.query(q,function(err,res){
   			if(err)return callback(err);
   			load(data,callback);
