de809ca404e7c189be731c023e3e8b267178ad8a
lintaba <lintaba+rpi@gmail.com>
2014-05-28 05:14:38 +0200

    asd

--- app/models/comment.js
+++ app/models/comment.js
@@ -7,18 +7,16 @@ var env = process.env.NODE_ENV || 'development'
  , config = require('../../config/config')[env]
   , utils = require('../../lib/utils')
 
-function s(t){return t.replace(/['\\]/g,"");}
+function s(t){return t&&t.replace?t.replace(/['\\]/g,""):"";}
 
-module.exports = {
 
- load:function load(data,callback){
-  	client.query("SELECT comment FROM comments WHERE uid='"+s(data.uid)+"' and ownid='"+s(data.ownid)+"'", function(err, result) {
-    if(err){console.log("0",err); return callback(err);}
+ var load=function load(data,callback){
+  	client.query("SELECT comment,uid FROM comments WHERE uid='"+s(data.uid)+"' and ownid='"+s(data.ownid)+"'", function(err, result) {
+  	  if(err){console.log("0",err); return callback(err);}
 	    	callback(0,result.rows[0]||{});
   	});
-    //callback(0,"work in progress, but id was "+data.uid+".");
-  },
-  save:function save(data,callback){
+  }
+var save=  function save(data,callback){
   	load(data,function(err,res){
   		if(err)return callback(err);
   		var q;
@@ -33,4 +31,4 @@ module.exports = {
   		})
   	})
   }
-}
+return module.exports = {load:load,save:save}
